c266b1252a33be8bc030d4cfba1f98f3c93bfcca
Alyssa Ross <hi@alyssa.is>
2021-12-09 05:14:15 +0000

    configuration.nix: remove GNOME+installer stuff
    Use cage, and a normal filesystem config.

--- configuration.nix
+++ configuration.nix
@@ -3,15 +3,27 @@
 
 { pkgs, ... }:
 
+let
+  inherit (builtins) readFile;
+in
+
 {
-  imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix>
-  ];
+  fileSystems."/" = { fsType = "tmpfs"; };
+  fileSystems."/nix/store" = {
+    device = "/dev/disk/by-partuuid/6e23b026-9f1e-479d-8a58-a0cda382e1ce";
+  };
+
+  services.cage.enable = true;
+  services.cage.program = "gnome-image-installer";
+  users.users.demo = { group = "demo"; isSystemUser = true; };
+  users.groups.demo = {};
+  security.polkit.extraConfig = readFile ./seat.rules;
 
   boot.loader.systemd-boot.enable = true;
 
   environment.systemPackages = with pkgs; [
     (callPackage ./installer.nix { })
+    gnome.adwaita-icon-theme
   ];
 
   systemd.tmpfiles.rules = [
