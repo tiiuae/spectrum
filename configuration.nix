e31a7b9d080b2e9deb83fcd30b0e0dbea26b12f3
Alyssa Ross <hi@alyssa.is>
2021-12-09 22:54:40 +0000

    vendor-customer-support.ini: extract from config
    Editing files is much easier outside of Nix config, especially if
    there's nothing dynamic in it.

--- configuration.nix
+++ configuration.nix
@@ -33,9 +33,6 @@ in
   ];
 
   systemd.tmpfiles.rules = [
-    "L+ /var/lib/eos-image-defaults/vendor-customer-support.ini - - - - ${pkgs.writeText "vendor-customer-support.ini" ''
-      [Customer Support]
-      Email = discuss@spectrum-os.org
-    ''}"
+    "L+ /var/lib/eos-image-defaults/vendor-customer-support.ini - - - - ${app/vendor-customer-support.ini}"
   ];
 }
