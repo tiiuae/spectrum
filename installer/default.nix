afe325db597aec5088b69579cd645585dbf35508
Alyssa Ross <hi@alyssa.is>
2021-12-16 17:28:58 +0000

    installer: use tar2ext4 to generate eosimages
    This way, we can avoid runInLinuxVM and the consequent slowness.
    Despite what its documentation implies, eos-installer doesn't require
    the eosimages partition to be exFAT.  Ideally, we'd use squashfs, but
    it looks up the filesystem by label, which squashfs doesn't support.

--- installer/default.nix
+++ installer/default.nix
@@ -49,20 +49,19 @@ let
   storeDev = config.fileSystems."/nix/store".device;
   installerUuid = head (match "/dev/disk/by-partuuid/(.*)" storeDev);
 
-  eosimages = vmTools.runInLinuxVM (runCommand "eosimages.img" {
-    nativeBuildInputs = [ exfatprogs kmod util-linux ];
+  eosimages = runCommand "eosimages.img" {
+    nativeBuildInputs = [ e2fsprogs tar2ext4 ];
+    imageName = "Spectrum-0.0-x86_64-generic.0.Live.img";
     passthru = { inherit image; };
   } ''
-    truncate -s 5G "$out"
-    mkfs.exfat -L eosimages "$out"
-    mkdir /mnt
-    modprobe loop
-    mount "$out" /mnt
-    name=Spectrum-0.0-x86_64-generic.0.Live.img
-    cp ${image} /mnt/$name
-    sha256sum /mnt/$name > /mnt/$name.sha256
-    umount /mnt
-  '');
+    mkdir dir
+    cd dir
+    ln -s ${image} Spectrum-0.0-x86_64-generic.0.Live.img
+    sha256sum $imageName > $imageName.sha256
+    tar -chf $NIX_BUILD_TOP/eosimages.tar *
+    tar2ext4 -i $NIX_BUILD_TOP/eosimages.tar -o $out
+    e2label $out eosimages
+  '';
 in
 
 runCommand "spectrum-installer" {
