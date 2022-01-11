6cf90f5eb290759ec2e0bb2dc07e44fa945742ec
Alyssa Ross <hi@alyssa.is>
2022-01-11 17:46:06 +0000

    shell.nix: add for top-level
    There's nothing to build at the top-level, but we can still benefit
    from a shell.nix for linters and stuff like reuse.

---
+++ shell.nix
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2022 Alyssa Ross <hi@alyssa.is>
+# SPDX-License-Identifier: MIT
+
+{ pkgs ? import <nixpkgs> {} }: with pkgs;
+
+mkShell {
+  nativeBuildInputs = [ reuse ];
+}
