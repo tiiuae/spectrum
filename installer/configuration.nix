907294e4a85e77e5db116a73ecfdf9a406be663b
Alyssa Ross <hi@alyssa.is>
2022-01-12 16:31:42 +0000

    installer: use plymouth
    We can't get flicker-free boot because of the GRUB menu, but this is a
    decent result that hides all the NixOS messages and puts up a Spectrum
    logo.  The overall installer size is only increased by 11.5MiB.

--- installer/configuration.nix
+++ installer/configuration.nix
@@ -10,7 +10,24 @@ in
 {
   imports = [ (modulesPath + "/profiles/all-hardware.nix") ];
 
+  boot.consoleLogLevel = 2;
+  boot.kernelParams = [ "udev.log_priority=5" ];
   boot.initrd.availableKernelModules = [ "squashfs" ];
+  boot.initrd.verbose = false;
+
+  boot.plymouth.enable = true;
+  boot.plymouth.logo = pkgs.callPackage (
+    { lib, runCommand, fetchurl, inkscape }:
+    runCommand "spectrum-logo.png" {
+      nativeBuildInputs = [ inkscape ];
+      svg = fetchurl {
+        url = "https://spectrum-os.org/git/www/plain/logo/logo_mesh.svg?id=5ac2d787b12e05a9ea91e94ca9373ced55d7371a";
+        sha256 = "1k5025nc5mxdls7bw7wk1734inadvibqxvg8b8yg6arr3y9yy46k";
+      };
+    } ''
+      inkscape -w 48 -h 48 -o "$out" "$svg"
+    ''
+  ) {};
 
   fileSystems."/" = { fsType = "tmpfs"; };
   fileSystems."/nix/store" = {
