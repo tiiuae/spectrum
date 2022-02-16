efe57ce3c04000cc94a4fc8444491147bf5a7c71
Alyssa Ross <hi@alyssa.is>
2022-02-16 21:46:54 +0000

    host/initramfs/extfs: drop s6-rc stuff
    This happens at runtime now.

--- host/initramfs/extfs.nix
+++ host/initramfs/extfs.nix
@@ -21,20 +21,9 @@ let
 in
 
 runCommand "ext.ext4" {
-  nativeBuildInputs = [ tar2ext4 s6-rc ];
+  nativeBuildInputs = [ tar2ext4 ];
 } ''
-  mkdir s6-rc svc
-
-  tar -C ${netvm}/s6-rc -c . | tar -C s6-rc -x
-  chmod +w s6-rc
-  tar -C ${appvm-catgirl}/s6-rc -c . | tar -C s6-rc -x
-  chmod +w s6-rc
-  tar -C ${appvm-lynx}/s6-rc -c . | tar -C s6-rc -x
-  chmod +w s6-rc
-  mkdir s6-rc/default
-  echo bundle > s6-rc/default/type
-  printf "appvm-catgirl\nappvm-lynx\n" > s6-rc/default/contents
-  s6-rc-compile svc/s6-rc s6-rc
+  mkdir svc
 
   tar -C ${netvm} -c data | tar -C svc -x
   chmod +w svc/data
