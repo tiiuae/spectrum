83fe8d9760bc36680c3294fa5f8c0ee7f45da7af
Alyssa Ross <hi@alyssa.is>
2022-02-20 12:39:12 +0000

    host/start-vm: add tests for tap_open

---
+++ host/start-vm/tests/tap_open-name-too-long.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: EUPL-1.2
+// SPDX-FileCopyrightText: 2022 Alyssa Ross <hi@alyssa.is>
+
+#include "../net-util.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <net/if.h>
+#include <string.h>
+
+int main(void)
+{
+	char name[IFNAMSIZ];
+	int fd;
+
+	memset(name, 'a', sizeof name);
+	fd = tap_open(name, 0);
+	assert(fd == -1);
+	assert(errno == ENAMETOOLONG);
+}
---
+++ host/start-vm/tests/tap_open.c
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: EUPL-1.2
+// SPDX-FileCopyrightText: 2022 Alyssa Ross <hi@alyssa.is>
+
+#include "../net-util.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <sched.h>
+
+int main(void)
+{
+	char name[] = "tap%d";
+	int fd;
+
+	unshare(CLONE_NEWUSER|CLONE_NEWNET);
+
+	fd = tap_open(name, 0);
+	if (fd == -1 && errno == EPERM)
+		return 77;
+	assert(fd != -1);
+}
