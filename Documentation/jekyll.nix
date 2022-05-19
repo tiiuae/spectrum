d4f5a5f730293e4c63bfbc1c16022ecf49935ce3
Alyssa Ross <hi@alyssa.is>
2022-05-19 09:15:08 +0000

    Documentation: don't use domain names in links

--- Documentation/jekyll.nix
+++ Documentation/jekyll.nix
@@ -28,6 +28,14 @@ bundlerApp {
             url = "https://github.com/just-the-docs/just-the-docs/compare/3a834d24ab1bda72f481f1e630f28fb9ba78ce64...e1a76ca3b6c74dfbb1d93f90484a69587e1b3804.patch";
             sha256 = "sha256-W39GTLL8wKMRakk/wa1hjjktbIGWhITWtAdbiMK3PI0=";
           })
+
+          # Don't use domains in links, which would require different
+          # configuration when running locally vs on the website.
+          # https://github.com/just-the-docs/just-the-docs/pull/544
+          (fetchpatch {
+            url = "https://github.com/just-the-docs/just-the-docs/commit/7bb40aa3c71d989339322ad946cfdd8287717a94.patch";
+            sha256 = "sha256-3e6N1B9lAgSYVRMLLvhsfOP5CXuespKJk/pyGdbK4wg=";
+          })
         ];
 
         postPatch = ''
