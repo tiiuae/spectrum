tiiuae/spectrum

commit 969842dd0b86922383b3b5e030650ad85da0e3a7

Use QEMU's built-in edk2 binaries
This saves the need to build OVMF separately, which has quite a long
dependency chain.

img/installer/run-vm.nix
@@ -5,7 +5,7 @@
 
 let
   inherit (builtins) storeDir;
-  inherit (pkgs) OVMF coreutils qemu_kvm writeShellScript;
+  inherit (pkgs) coreutils qemu_kvm writeShellScript;
   inherit (pkgs.lib) makeBinPath escapeShellArg;
 
   eosimages = import ../combined/eosimages.nix { inherit pkgs; };
@@ -36,9 +36,9 @@ writeShellScript "run-spectrum-installer-vm.sh" ''
     -display gtk,gl=on \
     -device virtio-vga-gl \
     -virtfs local,mount_tag=store,path=/nix/store,security_model=none,readonly=true \
+    -drive file=${qemu_kvm}/share/qemu/edk2-${stdenv.hostPlatform.qemuArch}-code.fd,format=raw,if=pflash,readonly=true \
     -drive file=${eosimages},format=raw,if=virtio,readonly=true \
     -drive file=/proc/self/fd/3,format=raw,if=virtio \
-    -bios ${OVMF.fd}/FV/OVMF.fd \
     -kernel ${installer.kernel} \
     -initrd ${installer.initramfs} \
     -append ${escapeShellArg installer.kernelParams}

img/combined/run-vm.nix
@@ -16,9 +16,9 @@ writeShellScript "run-spectrum-installer-vm.sh" ''
   exec qemu-kvm -cpu host -m 4G -machine q35 -snapshot \
     -display gtk,gl=on \
     -device virtio-vga-gl \
-    -bios ${OVMF.fd}/FV/OVMF.fd \
     -device qemu-xhci \
     -device usb-storage,drive=drive1,removable=true \
+    -drive file=${qemu_kvm}/share/qemu/edk2-${stdenv.hostPlatform.qemuArch}-code.fd,format=raw,if=pflash,readonly=true \
     -drive file=${image},id=drive1,format=raw,if=none,readonly=true \
     -drive file=/proc/self/fd/3,format=raw,if=virtio
 ''

img/live/Makefile
@@ -61,11 +61,11 @@ clean:
 
 run: build/live.img
 	$(QEMU_KVM) -m 4G \
-	    -bios $(OVMF_FD) \
 	    -cpu host \
 	    -display gtk,gl=on \
 	    -device virtio-vga-gl \
 	    -device qemu-xhci \
 	    -device usb-storage,drive=drive1,removable=true \
+	    -drive file=$(OVMF_CODE),format=raw,if=pflash,readonly=true \
 	    -drive file=build/live.img,id=drive1,format=raw,if=none,readonly=true
 .PHONY: run

img/live/shell.nix
@@ -8,5 +8,5 @@ with pkgs;
 (import ./. { inherit pkgs; }).overrideAttrs ({ nativeBuildInputs ? [], ... }: {
   nativeBuildInputs = nativeBuildInputs ++ [ qemu_kvm ];
 
-  OVMF_FD = "${OVMF.fd}/FV/OVMF.fd";
+  OVMF_CODE = "${qemu_kvm}/share/qemu/edk2-${stdenv.hostPlatform.qemuArch}-code.fd";
 })

commit e7ec26b28ca1643aaaadb99d57bd82e29b43c7a8

img/installer: fix run-vm.nix eval
stdenv is used to figure out the name of the EDK2 binary to run based
on the host architecture.

Fixes: 969842d ("Use QEMU's built-in edk2 binaries")

img/installer/run-vm.nix
@@ -5,7 +5,7 @@
 
 let
   inherit (builtins) storeDir;
-  inherit (pkgs) coreutils qemu_kvm writeShellScript;
+  inherit (pkgs) coreutils qemu_kvm stdenv writeShellScript;
   inherit (pkgs.lib) makeBinPath escapeShellArg;
 
   eosimages = import ../combined/eosimages.nix { inherit pkgs; };