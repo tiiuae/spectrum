d75786efd18de264b4c945fbee26db1cc343e1b2
Yuri Nesterov <nesteroff@users.noreply.github.com>
2022-08-26 10:55:44 +0300

    Fix live image for imx8
    * host/rootfs: change console device name from ttyS0 to ttyLP0
    * img/live: use aarch64 efi bootloader for the live image
    
    Signed-off-by: Yuri Nesterov <yuri.nesterov@gmail.com>

--- release/live/default.nix
+++ release/live/default.nix
@@ -36,8 +36,7 @@ stdenvNoCC.mkDerivation {
   INITRAMFS = initramfs;
   KERNEL = "${rootfs.kernel}/${stdenv.hostPlatform.linux-kernel.target}";
   ROOT_FS = rootfs;
-  SYSTEMD_BOOT_EFI = "${systemd}/lib/systemd/boot/efi/systemd-boot${efiArch}.efi";
-  EFINAME = "BOOT${toUpper efiArch}.EFI";
+  SYSTEMD_BOOT_EFI = "${systemd}/lib/systemd/boot/efi/systemd-bootaa64.efi";
 
   buildFlags = [ "build/live.img" ];
   makeFlags = [ "SCRIPTS=${scripts}" ];
