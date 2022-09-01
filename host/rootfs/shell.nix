6af16d04b71fd4a675e77dd707bc8e36513c8a85
Alyssa Ross <hi@alyssa.is>
2022-09-01 10:46:28 +0000

    host/rootfs: use initramfs in "make run"
    This will allow us to stop compiling e.g. the virtio-blk module into
    the kernel, because it will be loaded by the initramfs.
    
    This introduces some duplication between the rootfs and initramfs's
    Makefiles.  I don't think it's worth the effort at the moment to try
    to reduce that, because it would come at the expense of additional
    complexity in the Makefiles.  We can revisit this later if we want to.
    
    Signed-off-by: Alyssa Ross <hi@alyssa.is>
    Message-Id: <20220901104629.863380-1-hi@alyssa.is>
    Tested-by: José Pekkarinen <jose.pekkarinen@unikie.com>

--- host/rootfs/shell.nix
+++ host/rootfs/shell.nix
@@ -1,18 +1,24 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2021 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2022 Unikie
 
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  rootfs = import ./. { inherit pkgs; };
+in
+
 with pkgs;
 
-(import ./. { inherit pkgs; }).overrideAttrs (
+rootfs.overrideAttrs (
 { passthru ? {}, nativeBuildInputs ? [], ... }:
 
 {
   nativeBuildInputs = nativeBuildInputs ++ [
-    jq netcat qemu_kvm reuse util-linux
+    cryptsetup jq netcat qemu_kvm reuse util-linux
   ];
 
   EXT_FS = pkgsStatic.callPackage ../initramfs/extfs.nix { inherit pkgs; };
+  INITRAMFS = import ../initramfs { inherit pkgs rootfs; };
   KERNEL = "${passthru.kernel}/${stdenv.hostPlatform.linux-kernel.target}";
 })
