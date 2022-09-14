2cc5bf637f944ec950f7a8162018f421e6671315
Alyssa Ross <hi@alyssa.is>
2022-09-14 08:22:22 +0000

    shell.nix: automatically configure b4
    This will allow people who just want to test one patch series to skip
    an annoying setup process.  To be honest, I'm not sure if it's worth
    keeping the documentation about manually setting it up.  I'm worried
    it'll just confuse people.  But I'm keeping it for now.  We can remove
    it later.
    
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

--- shell.nix
+++ shell.nix
@@ -1,8 +1,16 @@
 # SPDX-FileCopyrightText: 2022 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2022 Unikie
 # SPDX-License-Identifier: MIT
 
 { config ? import nix/eval-config.nix {} }: with config.pkgs;
 
 mkShell {
-  nativeBuildInputs = [ reuse rustfmt ];
+  nativeBuildInputs = [ b4 reuse rustfmt ];
+
+  shellHook = ''
+    declare -igx GIT_CONFIG_COUNT
+    export "GIT_CONFIG_KEY_''${GIT_CONFIG_COUNT:-0}"=b4.midmask
+    export "GIT_CONFIG_VALUE_''${GIT_CONFIG_COUNT:-0}"=https://spectrum-os.org/lists/archives/spectrum-devel/%s
+    GIT_CONFIG_COUNT+=1
+  '';
 }
