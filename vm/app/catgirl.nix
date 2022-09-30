00d8fd1cf2df55804cf5683285603041cc57f050
Alyssa Ross <hi@alyssa.is>
2022-09-30 21:35:25 +0000

    img/app: don't block app startup on network online
    Not all VMs will even have networking!  So it makes more sense to put
    waiting for the network in the application-specific part.
    
    The "listen" and "wait" scripts are copied from the host system.
    
    Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>
    Message-Id: <20220930213533.1710618-3-alyssa.ross@unikie.com>

--- vm/app/catgirl.nix
+++ vm/app/catgirl.nix
@@ -10,6 +10,7 @@ import ../../vm-lib/make-vm.nix { inherit config; } {
     { writeScript, catgirl }:
     writeScript "run-catgirl" ''
       #!/bin/execlineb -P
+      if { /etc/mdev/wait network-online }
       foreground { printf "IRC nick (to join #spectrum): " }
       backtick -E nick { head -1 }
       ${catgirl}/bin/catgirl -h irc.libera.chat -j "#spectrum" -n $nick
