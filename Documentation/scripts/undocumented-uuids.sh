2fef3aa3b3821e29dfb56778da20bd0b5cf2ad7f
Henri Rosten <henri.rosten@unikie.com>
2022-11-07 09:00:27 +0200

    Remove bashisms from spectrum shell scripts
    This commit removes bashisms from spectrum shell scripts. This change is
    needed to be able to use the scripts from POSIX-compliant shells which
    are not bash compatible - such as dash.
    
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- Documentation/scripts/undocumented-uuids.sh
+++ Documentation/scripts/undocumented-uuids.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -eu
 # SPDX-FileCopyrightText: 2022 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2022 Unikie
 # SPDX-License-Identifier: EUPL-1.2+
 
 cd "$(dirname "$0")/../.."
@@ -7,8 +8,11 @@ cd "$(dirname "$0")/../.."
 PATTERN='\b[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}\b'
 UUID_REFERENCE_PATH=Documentation/uuid-reference.adoc
 
+tmp=$(mktemp)
+grep -Eio "$PATTERN" "$UUID_REFERENCE_PATH" | sort -u >$tmp
 git ls-files -coz --exclude-standard |
     grep -Fxvz "$UUID_REFERENCE_PATH" |
     xargs -0 git grep -Ehio --no-index --no-line-number "$PATTERN" -- |
     sort -u |
-    comm -23 - <(grep -Eio "$PATTERN" "$UUID_REFERENCE_PATH" | sort -u)
+    comm -23 - $tmp
+rm -f $tmp
