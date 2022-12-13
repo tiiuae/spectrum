5b397e45af0f923aea333ad03f26deedf0b19244
Tero Tervala <tero.tervala@unikie.com>
2022-12-13 17:30:51 +0200

    Point directly to config.nix to build on Hydra
    Signed-off-by: Tero Tervala <tero.tervala@unikie.com>

--- nix/eval-config.nix
+++ nix/eval-config.nix
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2022 Unikie
 
 { config ?
-  let customPath = builtins.tryEval <spectrum-config>; in
+  let customPath = builtins.tryEval <spectrum-config/config.nix>; in
   if customPath.success then import customPath.value
   else if builtins.pathExists ../config.nix then import ../config.nix
   else {}
